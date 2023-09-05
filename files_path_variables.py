b9449cebe543d359bfa11291593901cbf8ac055f
Odrec <renatogarita@gmail.com>
2023-09-05 16:20:55 +0200

    files paths upgraded

--- files_path_variables.py
+++ files_path_variables.py
@@ -2,13 +2,18 @@ import json
 from pathlib import Path
 
 #Load all necessary paths
-root_dir = Path("/media/odrec/TOSHIBA EXT/files_nlp_deeplecture_2/")
-pdf_dir = Path("/media/odrec/TOSHIBA EXT/Korpus PFDs/")
-corr_dir = Path(root_dir, "corr")
+root_dir = Path("/media/odrec/TOSHIBA EXT/")
+files_dir = Path(root_dir, "files_nlp_deeplecture_2/")
+pdf_dir = Path(root_dir, "Korpus PFDs/")
+metadata_dir = Path(files_dir, "csv")
+corr_dir = Path(files_dir, "corr")
 splitted_data_folder = Path(corr_dir,"all_cleaned_text")
 corrected_data_folder = Path(corr_dir,"all_cleaned_corr_text")
 manually_corrected_data_folder = Path(corr_dir,"all_cleaned_manually_corr_text")
 neighborhoods_dir = Path(corr_dir, "neighborhoods")
+
+#Path to all metadata file
+metadata_file = Path(metadata_dir, "all_codes.csv.current")
 
 #Path to rules file
 path_to_rules_file = Path(corr_dir, "rules_for_values.csv")
